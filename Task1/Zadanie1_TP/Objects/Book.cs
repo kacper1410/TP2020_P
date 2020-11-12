b9cf473b8994fe4b68fb84bb1fb3855f9dc125ad
Patryk Kolanek <marti3go@gmail.com>
2020-11-12 20:50:40 +0100

    Add tests for book

---
+++ Task1/Zadanie1_TP/Objects/Book.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Task1_TP.Objects
+{
+    public class Book
+    {
+        public Guid BookId { get; }
+        public string Title { get; private set; }
+        public string Author { get; private set; }
+        public CoverType CoverType { get; private set; }
+        public string Genre { get; private set; }
+
+        public Book(string title, string author, CoverType coverType, string genre)
+        {
+            this.BookId = Guid.NewGuid();
+            this.Title = title;
+            this.Author = author;
+            this.CoverType = coverType;
+            this.Genre = genre;
+        }
+    }
+
+    public enum CoverType
+    {
+        HardcoverCaseWrap,
+        Paperback,
+        HardcoverDustJacket,
+        Other
+    }
+}
