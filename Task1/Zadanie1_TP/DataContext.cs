4d34d7ade9ad1ea61193d5f2d6665b933763b3d9
Patryk Kolanek <marti3go@gmail.com>
2020-11-13 21:38:14 +0100

    Add DataContext

---
+++ Task1/Zadanie1_TP/DataContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+using Task1_TP.Objects;
+
+namespace Task1_TP
+{
+    class DataContext
+    {
+        public List<Client> Clients = new List<Client>();
+        public Dictionary<Guid, Book> Books = new Dictionary<Guid, Book>();
+        public ObservableCollection<Purchase> Purchuases = new ObservableCollection<Purchase>();
+        public List<BookState> BookStates = new List<BookState>();
+    }
+}
