51a3cebd29e4a4e63d9eda21e58732abd1ae1bbc
Kacper <kacper.swiercz00@gmail.com>
2020-12-31 17:02:09 +0100

    Change project to .NET Framework

---
+++ Task3/Task3Solution/Task3/Class1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using System.IO;
+
+namespace Task3
+{
+    public class Class1
+    {
+        public void method()
+        {
+
+            DataClasses1DataContext dataClasses1 = new DataClasses1DataContext();
+            //IEnumerable<string> names = from product in dataClasses1.Products
+            //                            where product.ProductNumber == "112"
+            //                            select product.Name;
+
+            IEnumerable<string> names = dataClasses1.Products.Where(p => p.ProductNumber == "112").Select(p => p.Name);
+
+            Product product1 = new Product();
+            product1.Name = "xd";
+            product1.ProductNumber = "112";
+            product1.ModifiedDate = DateTime.Now;
+            product1.SellStartDate = DateTime.Now;
+            product1.SellEndDate = DateTime.Now;
+            product1.DiscontinuedDate = DateTime.Now;
+            product1.SafetyStockLevel = 156;
+            product1.ReorderPoint = 12;
+
+            dataClasses1.Products.InsertOnSubmit(product1);
+
+            dataClasses1.SubmitChanges();
+
+            File.WriteAllText("names", String.Join("\n", names.ToArray()));
+
+        }
+    }
+}
---
+++ Task3/Task3Solution/Task3Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Task3Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Task3Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("0e04cbac-a5fa-4d8f-a572-22310fbd0546")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
