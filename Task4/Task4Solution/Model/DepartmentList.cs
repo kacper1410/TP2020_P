0498c76f3dc495e1b9976a683062d174b1d68f07
Patryk Kolanek <marti3go@gmail.com>
2021-01-23 12:59:14 +0100

    Add model

---
+++ Task4/Task4Solution/Model/DepartmentList.cs
@@ -0,0 +1,25 @@
+﻿using Logic;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+
+namespace Model
+{
+	public class DepartmentList : INotifyCollectionChanged
+	{
+		private List<DepartmentWrapper> Departments;
+		private Model Model;
+		public event NotifyCollectionChangedEventHandler CollectionChanged;
+
+		public DepartmentList()
+		{
+			Model = new Model();
+			Departments = (List<DepartmentWrapper>)Model.GetDepartments();
+		}
+
+		public void AddDepartment(string name, string groupName)
+		{
+			Model.AddDepartment(name, groupName);
+			Departments = (List<DepartmentWrapper>)Model.GetDepartments();
+		}
+	}
+}
---
+++ Task4/Task4Solution/Model/Model.cs
@@ -0,0 +1,40 @@
+﻿using Logic;
+using System.Collections.Generic;
+
+namespace Model
+{
+	public class Model
+	{
+		private IDataRepository DataRepository;
+
+		public Model()
+		{
+			DataRepository = new DataRepository();
+		}
+
+		public Model(IDataRepository dataRepository)
+		{
+			DataRepository = dataRepository;
+		}
+
+		public void AddDepartment(string name, string groupName)
+		{
+			DataRepository.AddDepartment(name, groupName);
+		}
+
+		public IEnumerable<DepartmentWrapper> GetDepartments()
+		{
+			return DataRepository.GetDepartments();
+		}
+
+		public void UpdateDepartment(int id, string name, string groupName)
+		{
+			DataRepository.UpdateDepartment(id, name, groupName);
+		}
+
+		public void DeleteDepartment(int id)
+		{
+			DataRepository.RemoveDepartment(id);
+		}
+	}
+}
